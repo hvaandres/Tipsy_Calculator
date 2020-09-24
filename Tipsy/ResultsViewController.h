ef1e02083731d1852bed54b0a2c5666f4d189027
Andres Haro <hvaandres@icloud.com>
2020-09-23 23:21:19 -0600

    New changes

---
+++ Tipsy/ResultsViewController.h
@@ -0,0 +1,17 @@
+//
+//  ResultsViewController.h
+//  Tipsy
+//
+//  Created by Alan Haro on 9/23/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ResultsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tipsy/ResultsViewController.m
@@ -0,0 +1,32 @@
+//
+//  ResultsViewController.m
+//  Tipsy
+//
+//  Created by Alan Haro on 9/23/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+#import "ResultsViewController.h"
+
+@interface ResultsViewController ()
+
+@end
+
+@implementation ResultsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
